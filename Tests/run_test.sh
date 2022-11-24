89bf145a43ab41deff57843d072719b1354af7c0
DuanYuFi <DuanYuFi@foxmail.com>
2022-11-24 02:11:22 +0000

    add usage of script run_test.sh

--- Tests/run_test.sh
+++ Tests/run_test.sh
@@ -1,7 +1,19 @@
-if [ "$2" != "nocompile" ]; then
-    g++ -o $1 $1.cpp -lSPDZ -I../ -I../mpir -L../ -L../local/lib ../local/lib/*.so -g -std=c++11
-fi
+if [ "$1" == "" ]; 
+then
+    echo "Usage:   $0 filename [norun|nocompile]"
+    echo ""
+    echo "Example: $0 dzkp-ring-Z2k"
+    echo "         $0 dzkp-ring-Z2k nocompile"
 
-if [ "$2" != "norun" ]; then
-    ./$1
+else
+
+    if [ "$2" != "nocompile" ]; 
+    then
+        g++ -o $1 $1.cpp -lSPDZ -I../ -I../mpir -L../ -L../local/lib ../local/lib/*.so -g -std=c++11
+    fi
+
+    if [ "$2" != "norun" ]; 
+    then
+        ./$1
+    fi
 fi
